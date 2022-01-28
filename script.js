5fb9db96e49d6eec6a1c3cb70c2283f5ff48f0cf
Roger <rhoang510@gmail.com>
2022-01-28 12:57:50 -0800

    Updating functionality

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const addBtn = document.querySelector("#addBtn");
 const container = document.querySelector(".container");
 const newBook = document.querySelector(".newBook");
 const addBookBtn = document.querySelector(".addBookBtn");
-const newBookContainer = document.querySelector(".newBookContainer")
+const newBookContainer = document.querySelector(".newBookContainer");
 let myLibrary = [];
 
 
@@ -27,60 +27,61 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
 }
 
-function clear() {
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    read.value = "";
+function removeBook(e) {
+    myLibrary.splice(parseInt(e.target.parentNode.dataset.number), 1);
 }
 
 function createBookCard() {
     const div = document.createElement("div");
+    const div2 = document.createElement("div");
     const newTitle = document.createElement("p");
     const newAuthor = document.createElement("p"); 
     const newPages = document.createElement("p"); 
+    const haveYouRead = document.createElement("p"); 
     const checkbox = document.createElement("input"); 
     const deleteBtn = document.createElement("button");
-
+    
     div.classList.add("bookCard");
+    div2.classList.add("haveRead");
     checkbox.classList.add("readBtn");
     deleteBtn.classList.add("deleteBtn");
-
+    
     newTitle.textContent = title.value;
-    newAuthor.textContent = author.value;
+    newAuthor.textContent = "By " + author.value;
     newPages.textContent = pages.value + " pages";
     deleteBtn.textContent = "Delete Book";
 
+    haveYouRead.textContent = "Read?";
     checkbox.type = "checkbox";
     checkbox.name = "read";
     checkbox.value = "read";
-
-    // if(!read.checked == true) {
-    //     newRead.textContent = "Read";
-    //     newRead.style.backgroundColor.green;
-    // } else if(!read.checked == false) {
-    //     newRead.textContent = "Not Read";
-    //     newRead.style.backgroundColor.red;
-    // }
+    checkbox.checked = read.checked;
 
     div.appendChild(newTitle);
     div.appendChild(newAuthor);
     div.appendChild(newPages);
-    div.appendChild(checkbox);
+    div2.appendChild(haveYouRead);
+    div2.appendChild(checkbox);
+    div.appendChild(div2);
     div.appendChild(deleteBtn);
     newBookContainer.appendChild(div);
+
+    deleteBtn.addEventListener("click", () => {
+        removeBook();
+        
+    });
 }
 
-myLibrary.forEach(item => {
+function showAddBookContainer() {
 
-})
+}
 addBookBtn.addEventListener("click", () => {
     if (title.value === "" || author.value === "" || pages.value === "") {
         return; 
     } else {
     addBookToLibrary();
     createBookCard();
-    clear();
+    form.reset();
     }
 });
 
